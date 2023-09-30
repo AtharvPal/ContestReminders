b40236ea5153948ace8d2736338815cfb44daea0
Atharv <32848150+AtharvPal@users.noreply.github.com>
2023-09-30 15:12:24 +0530

    added top bar + sync button

--- app/src/main/java/com/example/contestreminders/MainActivity.kt
+++ app/src/main/java/com/example/contestreminders/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
 
         var list_view = findViewById<RecyclerView>(R.id.contest_reminder_list)
 
